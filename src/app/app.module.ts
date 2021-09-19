0e769a68055f0533b8d63bf4f686ae0af3672c09
ojaoc <correia.jafonso@gmail.com>
2021-09-19 13:45:19 +0100

    Added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, ToolbarComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
