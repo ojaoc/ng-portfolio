72af0a8802ec831f8c1bc740f633943bf11a0085
ojaoc <correia.jafonso@gmail.com>
2021-09-28 23:32:42 +0100

    Added theme and scroll event to detect past header

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+  @ViewChild('outerElem', { read: ElementRef, static: false })
+  elementView: ElementRef;
 
-  constructor() { }
+  showBg: Boolean = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.showBg = this.getToolbarScrolledPastHeader();
   }
 
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll() {
+    const isScrolledPastHeader: Boolean = this.getToolbarScrolledPastHeader();
+    this.showBg = isScrolledPastHeader;
+
+    if (isScrolledPastHeader) {
+      console.log('swag');
+    }
+  }
+
+  private getHeaderHeightMinusToolbar(): Number {
+    return Math.abs(
+      (document.getElementById('header-container')?.offsetHeight ?? 0) -
+        this.elementView.nativeElement.offsetHeight
+    );
+  }
+
+  private getToolbarScrolledPastHeader(): Boolean {
+    return (
+      document.documentElement.scrollTop >= this.getHeaderHeightMinusToolbar()
+    );
+  }
 }
